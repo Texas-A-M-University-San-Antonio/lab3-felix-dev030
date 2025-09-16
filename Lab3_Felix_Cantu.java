4b48c5911960363ec594dd2779d288b16614dfd6
Felix Sanchez Cantu <fsanc024@jaguar.tamu.edu>
2025-09-16 00:22:44 +0000

    Finished lab and got a better understaning og troubleshooting.

--- Lab3_Felix_Cantu.java
+++ Lab3_Felix_Cantu.java
@@ -11,21 +11,20 @@ public class Lab3_Felix_Cantu
     public static void main(String[] args) 
     {    Scanner input = new Scanner(System.in);
 
-     double INITIAL_VALUE, A; // here i am assigning my three variables the double command to gert a more percise reading of value.
-     
      System.out.println("Please enter the initial value of your home. "); // here i am asking the coder to input their desired value for the initial value of the home
+     double INITIAL_VALUE, A; // here i am assigning my three variables the double command to gert a more percise reading of value.
      INITIAL_VALUE = input.nextDouble(); // this is where they will input their initial value when asked
 
+     System.out.println("Please enter the Intrest rates of your home. ");// here i am asking the coder to input their desired value for the interest rates of the home
      double INTEREST_RATES = input.nextDouble(); // here i am making it so when i input my interets rates, it will convert it to a decimal from a percent
      INTEREST_RATES = INTEREST_RATES/ 100.0; // this is where they will input their interest rates when asked
-     System.out.println("Please enter the Intrest rates of your home. ");// here i am asking the coder to input their desired value for the interest rates of the home
      
+     System.out.println("Please enter the ammount of years elapsed. ");//here i am asking the coder to input their desired value for the ammount of elapsed years of the home
      int ELAPSED_YEARS; // here i am assigning my elapsed years as the int rather than double 
      ELAPSED_YEARS = input.nextInt(); // this is where they will input the ammount of elapsed years when asked
-     System.out.println("Please enter the ammount of years elapsed. ");//here i am asking the coder to input their desired value for the ammount of elapsed yearsof the home
 
-     A = INITIAL_VALUE * (1 + INTEREST_RATES) + Math.pow(ELAPSED_YEARS, 1.0);
-     System.out.println("The final value of your home is $" + A);
+     A = INITIAL_VALUE * Math.pow(1 + INTEREST_RATES, ELAPSED_YEARS); // here is the equation to find the final interest rates. due to order of operations im asking to multiply the Initial value times 1 + interest rates rasied to t with the help of math.pow
+     System.out.println("The final value of your home is $" + A); // simple text that says what my final value for the home is
 
     }      
 }
